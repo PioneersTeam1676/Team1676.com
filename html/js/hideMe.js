b9b77f135c889b47522faea9f3efba0c0c409ae4
Matthew L <epicmjl@gmail.com>
2017-01-27 22:19:32 -0500

    js

--- html/js/hideMe.js
+++ html/js/hideMe.js
@@ -1,5 +1,13 @@
 window.show = function(elementId) { 
-   	var elements = document.getElementsByTagName("hideMe");
+   	var elements = document.getElementsByTagName("hideMeDivision");
+    	for (var i = 0; i < elements.length; i++)
+       		elements[i].className = "hidden";
+  
+    		document.getElementById(elementId).className = "";
+}
+
+window.show = function(elementId) { 
+   	var elements = document.getElementsByTagName("hideMeSponsor");
     	for (var i = 0; i < elements.length; i++)
        		elements[i].className = "hidden";
   
