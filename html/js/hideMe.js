af4f6c46d741b2adcfa728482aa1dc9bdd13447f
Matthew <epicmjl@gmail.com>
2017-01-30 10:06:58 -0500

    Fix javascript

--- html/js/hideMe.js
+++ html/js/hideMe.js
@@ -1,5 +1,13 @@
-window.show = function(elementId) { 
-   	var elements = document.getElementsByTagName("hideMe");
+window.showDivisions = function(elementId) { 
+   	var elements = document.getElementsByTagName("hideMeDivisions");
+    	for (var i = 0; i < elements.length; i++)
+       		elements[i].className = "hidden";
+  
+    		document.getElementById(elementId).className = "";
+}
+
+window.showSponsors = function(elementId) { 
+   	var elements = document.getElementsByTagName("hideMeSponsors");
     	for (var i = 0; i < elements.length; i++)
        		elements[i].className = "hidden";
   
