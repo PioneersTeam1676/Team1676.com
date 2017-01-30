01549c10b00b8fde1a0610bf9a04db1044cd9e08
Matthew <epicmjl@gmail.com>
2017-01-30 12:16:47 -0500

    Division Slider

--- html/js/hideMe.js
+++ html/js/hideMe.js
@@ -6,6 +6,14 @@ window.showDivisions = function(elementId) {
     		document.getElementById(elementId).className = "";
 }
 
+window.showDivisionsRight = function(elementId) { 
+   	var elements = document.getElementsByTagName("hideMeDivisionsRight");
+    	for (var i = 0; i < elements.length; i++)
+       		elements[i].className = "hidden";
+  
+    		document.getElementById(elementId).className = "";
+}
+
 window.showSponsors = function(elementId) { 
    	var elements = document.getElementsByTagName("hideMeSponsors");
     	for (var i = 0; i < elements.length; i++)
